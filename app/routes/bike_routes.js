20c0485a31f2629e8cdfd519fbaf3a0a382ef66e
jnr0790 <jromero0790@gmaill.com>
2021-04-21 15:33:44 -0400

    Add create bike function

---
+++ app/routes/bike_routes.js
@@ -0,0 +1,46 @@
+// require express
+const express = require('express')
+
+// require passport for requireToken
+const passport = require('passport')
+
+// require bike schema
+const Bike = require('./../models/bike')
+
+// require custom error messages
+const customErrors = require('../../lib/custom_errors')
+
+// variable calling the handle404 method for handle404 function
+const handle404 = customErrors.handle404
+
+// requireOwnership messaging for someone trying to access another persons bike
+const requireOwnership = customErrors.requireOwnership
+
+// require middleware that will remove blank fields from `req.body`
+const removeBlanks = require('../../lib/remove_blank_fields')
+
+// make variable for requireToken for users
+const requireToken = passport.authenticate('bearer', { session: false })
+
+// use the router middleware as just router
+const router = express.Router()
+
+// CREATE
+// POST /bikes
+router.post('/bikes', requireToken, (req, res, next) => {
+  // set variable bikeData to req.body.bike
+  const bikeData = req.body.bike
+  // set owner of new bike to the current user
+  bikeData.owner = req.user.id
+
+  Bike.create(bikeData)
+    // respond to create with 201 if created correctly
+    .then(bike => {
+      res.status(201).json({ bike: bike.toObject() })
+    })
+    // if an error pass it to tour error handler
+    .catch(next)
+})
+
+// export router
+module.exports = router
---
+++ curl-scripts/bikes/create.sh
@@ -0,0 +1,18 @@
+API="http://localhost:4741"
+URL_PATH="/bikes"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "bike": {
+      "brand": "'"${BRAND}"'",
+      "model": "'"${MODEL}"'",
+      "type": "'"${TYPE}"'",
+      "color": "'"${COLOR}"'"
+    }
+  }'
+
+echo
